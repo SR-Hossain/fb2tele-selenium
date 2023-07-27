32a3ada33028e0154bc869208bcad31e94b76e89
SR-Hossain <65627729+SR-Hossain@users.noreply.github.com>
2023-07-27 03:43:14 +0000

    Version 38

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+import flask
+from threading import Thread
+from waitress import serve
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+
+def runApp():
+  
+  serve(app, host="0.0.0.0", port=8080)
+  #app.run(host='0.0.0.0',port=8080)
+
+def run():
+  Thread(target=runApp).start()
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fb import wait_in_unread_for_new_posts
 from login import login
 from scrap import send_new_posts_to_telegram
 # import time
+from keep_alive import run
 
 print('initiating..')
 login()
+run()
 while True:
     # print('sending to telegram...')
     send_new_posts_to_telegram()
